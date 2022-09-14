3a484cae02360a08c4c0d52e1ea980952f2f88ff
Alex Zenin <alexander.zenin@reece.com.au>
2022-09-14 15:25:40 +1000

    feat: update form fields

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,6 +52,21 @@ const SignupForm = () => {
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
+            id="company"
+            htmlFor="company"
+          >
+            Company
+          </label>
+          <input
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="company"
+            type="text"
+            placeholder="ABC Plumbing"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
             id="email"
             htmlFor="email"
           >
@@ -64,50 +79,13 @@ const SignupForm = () => {
             placeholder="example@reece.com"
           />
         </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            id="mobile"
-            htmlFor="mobile"
-          >
-            Mobile
-          </label>
-          <input
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="mobile"
-            type="number"
-            placeholder="0410 000 000"
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            id="password"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="******************"
-          />
-          {/* <p className="text-xs italic">Please choose a password.</p> */}
-        </div>
         <div className="flex items-center justify-between">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
             type="submit"
           >
-            Sign In
+            Opt In
           </button>
-          <a
-            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            href="#"
-          >
-            Forgot Password?
-          </a>
         </div>
       </form>
     </div>
