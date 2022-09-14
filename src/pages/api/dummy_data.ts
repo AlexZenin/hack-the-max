5e47c44031735507d81f9f5b7e31eaf1e10abf29
Bella Lafrenais <Bella.Lafrenais@reece.com.au>
2022-09-14 23:25:20 +1000

    add in s3 urls in the dummy data

--- src/pages/api/dummy_data.ts
+++ src/pages/api/dummy_data.ts
@@ -10,7 +10,7 @@ const DUMMY_DATA = [
     accountName: 'ABC Plumbing',
     accountNumber: '3133569',
     imgSrc:
-      'https://trsuat08.reecenet.org/point-of-sale/static/media/reece-logo.2f37b2d6.svg',
+      'https://guess-who-hackathon.s3.ap-southeast-2.amazonaws.com/1659376825003.jpeg',
     location: {
       lat: -37.83001846475084,
       lng: 144.99322631118585,
@@ -21,7 +21,7 @@ const DUMMY_DATA = [
     accountName: 'Blue Plumbing',
     accountNumber: '3133569',
     imgSrc:
-      'https://trsuat08.reecenet.org/point-of-sale/static/media/reece-logo.2f37b2d6.svg',
+      'https://guess-who-hackathon.s3.ap-southeast-2.amazonaws.com/Screen+Shot+2022-09-14+at+7.32.09+pm.png',
     location: {
       lat: -37.83001846475084,
       lng: 144.99322631118585,
@@ -32,7 +32,7 @@ const DUMMY_DATA = [
     accountName: 'Quick Plumbing',
     accountNumber: '3133569',
     imgSrc:
-      'https://trsuat08.reecenet.org/point-of-sale/static/media/reece-logo.2f37b2d6.svg',
+      'https://guess-who-hackathon.s3.ap-southeast-2.amazonaws.com/debugmeme.jpeg',
     location: {
       lat: -37.83001846475084,
       lng: 144.99322631118585,
@@ -54,7 +54,7 @@ const DUMMY_DATA = [
     accountName: 'BBC Plumbing',
     accountNumber: '3133569',
     imgSrc:
-      'https://trsuat08.reecenet.org/point-of-sale/static/media/reece-logo.2f37b2d6.svg',
+      'https://guess-who-hackathon.s3.ap-southeast-2.amazonaws.com/kanye.jpeg',
     location: {
       lat: -37.83001846475084,
       lng: 144.99322631118585,
